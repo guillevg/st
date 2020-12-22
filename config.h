5b33e34eb42a040c4bccf6e705da732ab1a96e85
Guille <gvicente@gmail.com>
2020-12-22 13:06:38 +0100

    Fix bg color

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -121,14 +121,11 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-//<<<<<<< ours
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
-//=======
-	"#cccccc",
-	"#555555",
-	"black",
-//>>>>>>> theirs
+	"#2e3440", /* 256 -> background */
+	"#d8dee9", /* 257 -> foreground */
+	"#112222",   /* 258 ->     */
+	"#cccccc", /* 259 ->     */
+	"#555555", /* 260 ->     */
 };
 
 
@@ -136,18 +133,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-/*<<<<<<< ours
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
-=======
-*/
-unsigned int defaultfg = 7;
 unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-//>>>>>>> theirs
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 260;
 
 /*
  * Default shape of cursor
