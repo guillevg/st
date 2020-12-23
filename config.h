e61bc9cbc402c3a4e72a8d7e509cfe4195bed7e1
Guille <gvicente@gmail.com>
2020-12-23 16:56:07 +0100

    Add mouse scroll (without shift)

--- config.h
+++ config.h
@@ -183,8 +183,8 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, kscrollup,      {.i = 3} },
 	{ ShiftMask,            Button5, kscrolldown,    {.i = 3} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 3} },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 3} },
 };
 
 /* Internal keyboard shortcuts. */
